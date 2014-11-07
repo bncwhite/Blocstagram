ea76163cd0f050464dd12b35c82b30d4ee558396
bncwhite <brad@codebybrad.com>
2014-11-07 17:13:01 -0600

    Halfway through Adding Cool Filters and Sending to Instagram, app crashes after adding noir filter and being told to run the app

---
+++ BLCPostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  BLCPostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Bradley White on 11/7/14.
+//  Copyright (c) 2014 Bradley White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCPostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
